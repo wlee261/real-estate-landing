af00b9688c1117f78776957f4c6c004445e1c9c2
Winston Lee <wlee@ucsb.edu>
2023-06-09 16:15:21 -0700

    added background image to valuation page

--- app/valuation/page.js
+++ app/valuation/page.js
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
-  return <div>Find out what your home is really worth</div>;
+  return (
+    <div>
+      <div className="valuation--background-image-container">
+        <Image
+          className="valuation--background-image"
+          src={"/house-interior.jpg"}
+          fill
+          alt="house background image"
+        />
+      </div>
+      Find out what your home is really worth
+    </div>
+  );
 };
 
 export default page;
